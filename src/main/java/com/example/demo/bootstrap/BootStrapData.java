2ace5e9385f2eb1a72fe298bd8cee7063d685f9f
hmalyala <m.hemanthkumar27@gmail.com>
2023-02-05 10:37:20 -0500

    working copy

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package com.example.demo.bootstrap;
 
 import com.example.demo.domain.Author;
 import com.example.demo.domain.Book;
+import com.example.demo.domain.Publisher;
 import com.example.demo.repositories.AuthorRepository;
 import com.example.demo.repositories.BookRepository;
+import com.example.demo.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,26 +14,47 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        System.out.println("Started in bootStrap");
+
+        Publisher publisher = new Publisher("SFG Publishing","St Petersburg",null, "FL", "32806");
+        publisherRepository.save(publisher);
         Author eric = new Author("Eric","Evans");
         Book ddd = new Book("Domain Driven Design","123123");
 
         eric.getBooks().add(ddd);
-        ddd.getAuthor().add(eric);
+        ddd.getAuthors().add(eric);
+        ddd.setPublisher(publisher);
+        publisher.getBooks().add(ddd);
 
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(publisher);
 
-        System.out.println("Started in bootStrap");
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "3939459459");
+
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+        noEJB.setPublisher(publisher);
+        publisher.getBooks().add(noEJB);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+        publisherRepository.save(publisher);
+
         System.out.println("Number of books: "+bookRepository.count());
+        System.out.println("Number of publishers: "+publisherRepository.count());
     }
 
 
--- src/main/java/com/example/demo/domain/Book.java
+++ src/main/java/com/example/demo/domain/Book.java
@@ -2,6 +2,7 @@ package com.example.demo.domain;
 
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -23,10 +24,22 @@ public class Book {
     private String bookName;
     private String isbn;
 
+    @ManyToOne(cascade = {CascadeType.MERGE })
+    @JoinColumn(name="publisher_id")
+    private Publisher publisher;
+
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name="book_id"),
             inverseJoinColumns = @JoinColumn(name = "author_id"))
-    private Set<Author> authors;
+    private Set<Author> authors = new HashSet();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getBookName() {
         return bookName;
@@ -44,8 +57,16 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public Set<Author> getAuthor() {
+    public Set<Author> getAuthors() {
         return authors;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 
     public void setAuthor(Set<Author> authors) {
